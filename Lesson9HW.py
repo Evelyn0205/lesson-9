859c589246dbf16513f648cb6ff8cc462c480b14
Evelyn0205 <86942627+Evelyn0205@users.noreply.github.com>
2021-07-15 18:50:04 +0800

    ?
    I took a billion years to finish it...  : )

---
+++ Lesson9HW.py
@@ -0,0 +1,23 @@
+commodity1 = {'A001':'gummy bear'}
+commodity2 = {'A002':'ice pop'}
+commodity3 = {'A004':'prince noodle'}
+commodity4= {'A006':'soda'}
+print(commodity 1,2,3,4)
+
+x = int(input('which one do you want to buy?'))
+
+1 = A001
+if 1 == x:
+    print('gummy bear $20')
+    
+2 = A002 
+elif 2 == x:
+    print('ice pop $25')
+    
+3 == A004
+elif 3 == x:
+    print('prince noodle $10')
+    
+4 == A006
+elif 4 == x:
+    print('soda $30')
